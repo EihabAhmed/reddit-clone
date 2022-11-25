93338349d85f4952ad73d5b808059c08deec15fc
EihabAhmed <eihabahmed@gmail.com>
2022-11-26 00:37:02 +0200

    Added signup and verification email (not sent, problem of the mail server)

---
+++ src/main/java/com/bbk/redditclone/exceptions/SpringRedditException.java
@@ -0,0 +1,7 @@
+package com.bbk.redditclone.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
--- src/main/java/com/bbk/redditclone/repository/VerificationTokenRepository.java
+++ src/main/java/com/bbk/redditclone/repository/VerificationTokenRepository.java
@@ -1,6 +1,12 @@
 package com.bbk.redditclone.repository;
 
-public interface VerificationTokenRepository {
-}
+import com.bbk.redditclone.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-10:00
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+}
